e12a450e6a38802ca94af2b09a526274984b8028
jaroszej <jason.j@rosze.dev>
2023-05-08 17:15:58 -0400

    improved start script

--- start.bat
+++ start.bat
@@ -1,1 +1,8 @@
+@echo off
 java -jar skimmands-uploader.jar > skimmands-uploader-log.txt
+
+echo.^
+ & echo [ATTENTION] You may close this window. Log output is available at skimmands-uploader-log.txt
+echo.^
+
+pause
