fe36e64bbbcc3eeb2c605c2f0cc449cb0e6cd1da
jaroszej <jason.j@rosze.dev>
2023-05-08 21:39:48 -0400

    Log fixes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,6 +21,8 @@ fun main() {
 
         // Set up logger
         val logger = Logger.getLogger("skimmands")
+
+        logger.info("Initializing...")
         logger.useParentHandlers = false
 
         // Create file handler to write to log files
@@ -35,15 +37,17 @@ fun main() {
         logger.addHandler(fileHandler)
 
         // Log startup message
-        logger.info("Starting skimmands...")
+        logger.info("Starting the Skimmands Uploader!")
 
         val syncInterval = System.getProperty("syncInterval")?.toInt() ?: 15
         val sqlitePath = System.getProperty("sqlitePath") ?: "config/phantombot.db"
         val mongoConnStr = System.getProperty("mongoConnStr") ?: "mongodb://localhost:27017"
 
-        logger.info(">sync interval: $syncInterval")
-        logger.info(">sqlite path: $sqlitePath")
-        logger.info(">mongodb uri: $mongoConnStr")
+        logger.info("### CONFIGURATION PARAMETERS: ###")
+        logger.info("sync interval: $syncInterval")
+        logger.info("sqlite path: $sqlitePath")
+        logger.info("mongodb uri: $mongoConnStr")
+        logger.info("#################################")
 
         // Set up timer to execute the retrieval and insertion at user-set intervals
         val timer = Timer()
@@ -76,47 +80,47 @@ fun main() {
                 // sets /////////
                 // only in SQLite -> create
                 val onlyInSQLite = sqliteData.filter { it.first !in mongoDataKeys }
-                logger.info(">sqlite only: $onlyInSQLite")
+                logger.info("> sqlite only: $onlyInSQLite")
 
                 // only in Mongo -> delete
                 val onlyInMongo = mongoData.filter { it.first !in sqlDataKeys }
-                logger.info(">mongo only: $onlyInMongo")
+                logger.info("> mongo only: $onlyInMongo")
 
                 // in SQLite and Mongo but have different values -> update
                 val matchingKeys = sqliteData.filter { it.first in mongoDataKeys && it.second !in mongoDataValues }
-                logger.info(">shared key different value: $matchingKeys")
+                logger.info("> shared key different value: $matchingKeys")
 
 
                 // CREATE
                 if (onlyInSQLite.isNotEmpty()) {
-                    logger.info(">>adding $onlyInSQLite to mongoDB")
+                    logger.info(">> adding $onlyInSQLite to mongoDB")
                     val createSuccess = mongoConn.createData(onlyInSQLite)
                     if (createSuccess) {
-                        logger.info(">>added ${onlyInSQLite.size} commands $onlyInSQLite to mongoDB")
+                        logger.info(">> added ${onlyInSQLite.size} commands $onlyInSQLite to mongoDB")
                     } else {
-                        logger.warning("Error creating data in MongoDB")
+                        logger.severe(">> Error creating data in MongoDB")
                     }
                 }
 
                 // UPDATE
                 if (matchingKeys.isNotEmpty()) {
-                    logger.info(">>updating $matchingKeys in mongoDB")
+                    logger.info(">> updating $matchingKeys in mongoDB")
                     val updateSuccess = mongoConn.updateData(matchingKeys)
                     if (updateSuccess) {
-                        logger.info(">>updated $matchingKeys in mongoDB")
+                        logger.info(">> updated $matchingKeys in mongoDB")
                     } else {
-                        logger.warning("Error updating data in MongoDB")
+                        logger.severe(">> Error updating data in MongoDB")
                     }
                 }
 
                 // DELETE
                 if (onlyInMongo.isNotEmpty()) {
-                    logger.info(">>removing $onlyInMongo from mongoDB")
+                    logger.info(">> removing $onlyInMongo from mongoDB")
                     val success = mongoConn.deleteData(onlyInMongo)
                     if (success) {
-                        logger.info(">>removed $onlyInMongo from mongoDB")
+                        logger.info(">> removed $onlyInMongo from mongoDB")
                     } else {
-                        logger.warning(">>failed to remove $onlyInMongo from mongoDB")
+                        logger.severe(">> failed to remove $onlyInMongo from mongoDB")
                     }
                 }
 
